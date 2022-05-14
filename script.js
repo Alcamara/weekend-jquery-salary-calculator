573aa927c6a6c2f5d1737ca916d74043c749a53d
Al Camara <Alhousseinec@gmail.com>
2022-05-13 23:32:11 -0500

    create employees array and employeeFactory function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-console.log('script');
+// console.log('script');
+
+// Create  empty array
+let employees = [];
 
 $(document).ready(readyNow)
 
 function readyNow(){
     console.log('JQ');
+}
+
+
+/* 
+Create employee data function 
+function should have 6 parameters
+return all data as an object
+*/
+
+let employeeFactory = (firstName, lastName, idNumber, title, salary) => {
+
+    return {
+        firstName : firstName,
+        lastName : lastName,
+        idNumber :idNumber,
+        title : title,
+        salary : salary
+
+    }
 }
